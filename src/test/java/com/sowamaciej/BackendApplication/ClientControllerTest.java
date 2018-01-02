231b039a966b76beac3027aac79ad4aa8f555edd
Maciej Sowa <sowa.maciej@outlook.com>
2018-01-02 16:22:02 +0100

    Added tests for Client model

---
+++ src/test/java/com/sowamaciej/BackendApplication/ClientControllerTest.java
@@ -0,0 +1,100 @@
+package com.sowamaciej.BackendApplication;
+
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.nio.charset.Charset;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientControllerTest {
+
+    private final MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+    private final static String NAME = "name";
+    private final static String PATH = "/clients";
+
+    private Client client;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ClientController clientController;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Before
+    public void setUP() {
+        mockMvc = MockMvcBuilders.standaloneSetup(clientController).build();
+        client = clientController.createClient(new Client(1, "name", "lastName", "12/12/1244", "male", 123));
+
+    }
+
+    @Test
+    public void shouldFindAllClients() throws Exception {
+        mockMvc.perform(get(PATH))
+                .andExpect(content().contentType(contentType))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(clientController.users().size())));
+    }
+
+    @Test
+    public void shouldFindClientById() throws Exception {
+        mockMvc.perform(get(PATH + "/" + client.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.id", is(client.getId().intValue())));
+
+    }
+
+    @Test
+    public void shouldCreateClient() throws Exception {
+        mockMvc.perform(post(PATH)
+                .contentType(contentType)
+                .content(objectMapper.writeValueAsString(client)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType));
+    }
+
+    @Test
+    public void shouldDeleteClient() throws Exception {
+        mockMvc.perform(delete(PATH + "/" + client.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.id", is(client.getId().intValue())));
+
+
+    }
+
+    @Test
+    public void shouldUpdateClient() throws Exception {
+        Client updatedClient = clientController.createClient(new Client(1, "name" + 2, "lastName" + 3, "12/12/1244", "male", 123));
+
+        mockMvc.perform(put(PATH + "/" + client.getId())
+                .contentType(contentType)
+                .content(objectMapper.writeValueAsString(updatedClient)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.name", is(updatedClient.getName())))
+                .andExpect(jsonPath("$.lastName", is(updatedClient.getLastName())));
+    }
+
+}
---
+++ src/test/java/com/sowamaciej/BackendApplication/ClientServiceTest.java
@@ -0,0 +1,76 @@
+package com.sowamaciej.BackendApplication;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collections;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+public class ClientServiceTest {
+
+    private Client client;
+
+    @MockBean
+    private ClientService clientService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        client = new Client(1, "name", "lastName", "12/12/1244", "male", 123);
+
+    }
+
+    @Test
+    public void shouldFindAllClients() {
+
+        when(clientService.findAllClients()).thenReturn(Collections.emptyList());
+        List<Client> clients = clientService.findAllClients();
+        assertTrue(clients.isEmpty());
+    }
+
+    @Test
+    public void shouldCreateClient() {
+        when(clientService.create(client)).thenReturn(client);
+        Client createdClient = client;
+        assertEquals("name", createdClient.getName());
+        assertEquals("lastName", createdClient.getLastName());
+    }
+
+    @Test
+    public void shouldFindClientById() {
+        when(clientService.findById(client.getId())).thenReturn(client);
+        Client currentClient = clientService.findById(client.getId());
+        Assert.assertNotNull(currentClient);
+    }
+
+    @Test
+    public void shouldUpdateClient() {
+        Client updateClient = new Client(1, "name" + "N", "lastName" + "B", "12/12/1244", "male", 123);
+        when(clientService.update(client.getId(), updateClient)).thenReturn(updateClient);
+        Client currentClient = clientService.update(client.getId(), updateClient);
+        assertEquals("name" + "N", currentClient.getName());
+        assertEquals("lastName" + "B", currentClient.getLastName());
+    }
+
+    @Test
+    public void shouldDeleteClientById() {
+        when(clientService.deleteClient(client.getId())).thenReturn(client);
+        Client deletedClient = clientService.deleteClient(client.getId());
+        assertEquals("name", deletedClient.getName());
+        assertEquals("male", deletedClient.getSex());
+
+    }
+
+}
